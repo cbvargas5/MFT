21ca8ee117dfb2984da76d3f537aa535464ff004
Darth Varg <cbvargas123@gmail.com>
2020-06-08 23:54:00 -0700

    Added App.jsx file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import App from './components/App.jsx'
 
-ReactDom.render(<h1>Rendering MFT w/ React</h1>, document.getElementById('root'))
+ReactDom.render(<App />, document.getElementById('root'))
