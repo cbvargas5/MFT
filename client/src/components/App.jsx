29e8f1020c1c8c72c2df52ea7e5bae37b326d543
Darth Varg <cbvargas123@gmail.com>
2020-06-10 23:53:11 -0700

    added more component files; imported Contact component to App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import NavBar from './NavBar.jsx'
+import Contact from './Contact.jsx'
+
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Contact />
     </div>
   )
 }
