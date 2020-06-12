56ace62e270d8013f02c36809253cce77bcdc4a1
Darth Varg <cbvargas123@gmail.com>
2020-06-11 23:53:39 -0700

    created form skeleton for Contact component

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <form>
+        <label>Name</label>
+        <input></input>
+        <label>Email*</label>
+        <input></input>
+        <label>Subject</label>
+        <input></input>
+        <label>Message</label>
+        <input></input>
+      </form>
     </div>
   )
 }
