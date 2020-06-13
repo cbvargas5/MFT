6f443dda6c1eee951aafbab29b266be5647ae407
Darth Varg <cbvargas123@gmail.com>
2020-06-12 21:49:33 -0700

    modified Contact component by adding info to contact and seperating component into 3 sections

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
   return (
     <div>
-      <form>
-        <label>Name</label>
-        <input></input>
-        <label>Email*</label>
-        <input></input>
-        <label>Subject</label>
-        <input></input>
-        <label>Message</label>
-        <input></input>
-        <button>Send</button>
-      </form>
+      <h1 className="contact-title">CONTACT</h1>
+      <div className="contact-info-wrapper">
+        <h2>Nikki Beecher</h2>
+        <h3>Nevada Licensed Marriage and Family Therapist Intern</h3>
+        <a className="fb-link" href="https://www.facebook.com/mftnikki/" target="_blank"><span>Caritas Counseling on Facebook</span></a>
+      </div>
+      <div className="contact-form-wrapper">
+        <form>
+          <label>Name</label>
+          <input></input>
+          <label>Email *</label>
+          <input></input>
+          <label>Subject</label>
+          <input></input>
+          <label>Message</label>
+          <input></input>
+          <button>Send</button>
+        </form>        
+      </div>
     </div>
   )
 }
